0f649fc299036bfc717210afd9e2e5c45793245f
gouzuoluo <1046292959@qq.com>
2022-12-04 11:22:19 +0800

    标准化

--- 10.utils/util.go
+++ 10.utils/util.go
@@ -13,8 +13,7 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-// 检查地址是否是有效的以太坊地址
-// IsValidAddress validate hex address
+// IsValidAddress 检查地址是否是有效的以太坊地址
 func IsValidAddress(iaddress interface{}) bool {
 	re := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
 	switch v := iaddress.(type) {
@@ -27,8 +26,7 @@ func IsValidAddress(iaddress interface{}) bool {
 	}
 }
 
-// 检查地址是否为零地址
-// IsZeroAddress validate if it's a 0 address
+// IsZeroAddress 检查地址是否为零地址
 func IsZeroAddress(iaddress interface{}) bool {
 	var address common.Address
 	switch v := iaddress.(type) {
@@ -45,11 +43,10 @@ func IsZeroAddress(iaddress interface{}) bool {
 	return reflect.DeepEqual(addressBytes, zeroAddressBytes)
 }
 
-// 将小数转换为 wei(整数）。 第二个参数是小数位数
-// ToDecimal wei to decimals
-func ToDecimal(ivalue interface{}, decimals int) decimal.Decimal {
+// ToDecimal 将小数转换为 wei(整数）。第二个参数是小数位数
+func ToDecimal(iValue interface{}, decimals int) decimal.Decimal {
 	value := new(big.Int)
-	switch v := ivalue.(type) {
+	switch v := iValue.(type) {
 	case string:
 		value.SetString(v, 10)
 	case *big.Int:
@@ -63,11 +60,10 @@ func ToDecimal(ivalue interface{}, decimals int) decimal.Decimal {
 	return result
 }
 
-// 将wei（整数）转换为小数。 第二个参数是小数位数。
-// ToWei decimals to wei
-func ToWei(iamount interface{}, decimals int) *big.Int {
+// ToWei 将 wei（整数）转换为小数。 第二个参数是小数位数。
+func ToWei(iAmount interface{}, decimals int) *big.Int {
 	amount := decimal.NewFromFloat(0)
-	switch v := iamount.(type) {
+	switch v := iAmount.(type) {
 	case string:
 		amount, _ = decimal.NewFromString(v)
 	case float64:
@@ -89,32 +85,30 @@ func ToWei(iamount interface{}, decimals int) *big.Int {
 	return wei
 }
 
-// 根据燃气上限和燃气价格计算燃气花费。
-// CalcGasCost calculate gas cost given gas limit (units) and gas price (wei)
+// CalcGasCost 根据燃气上限和燃气价格计算燃气花费。
 func CalcGasCost(gasLimit uint64, gasPrice *big.Int) *big.Int {
 	gasLimitBig := big.NewInt(int64(gasLimit))
 	return gasLimitBig.Mul(gasLimitBig, gasPrice)
 }
 
-// 从签名中提取 R，S 和 V 值。
-// SigRSV signatures R S V returned as arrays
-func SigRSV(isig interface{}) ([32]byte, [32]byte, uint8) {
+// SigRSV 从签名中提取 R，S 和 V 值。
+func SigRSV(iSig interface{}) ([32]byte, [32]byte, uint8) {
 	var sig []byte
-	switch v := isig.(type) {
+	switch v := iSig.(type) {
 	case []byte:
 		sig = v
 	case string:
 		sig, _ = hexutil.Decode(v)
 	}
 
-	sigstr := common.Bytes2Hex(sig)
-	rS := sigstr[0:64]
-	sS := sigstr[64:128]
+	sigStr := common.Bytes2Hex(sig)
+	rS := sigStr[0:64]
+	sS := sigStr[64:128]
 	R := [32]byte{}
 	S := [32]byte{}
 	copy(R[:], common.FromHex(rS))
 	copy(S[:], common.FromHex(sS))
-	vStr := sigstr[128:130]
+	vStr := sigStr[128:130]
 	vI, _ := strconv.Atoi(vStr)
 	V := uint8(vI + 27)
 
