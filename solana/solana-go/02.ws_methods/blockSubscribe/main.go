f0be960bf8b3d3bb83f315cd8aa50457364e47d0
gouzuoluo <1046292959@qq.com>
2024-05-08 14:21:48 +0800

    solana

--- solana/solana-go/02.ws_methods/blockSubscribe/main.go
+++ solana/solana-go/02.ws_methods/blockSubscribe/main.go
@@ -9,16 +9,19 @@ import (
 	"github.com/gagliardetto/solana-go/rpc/ws"
 )
 
+// 此订阅被视为不稳定，仅当验证程序使用 --rpc-pubsub-enable-block-subscription 标志启动时才可用。此订阅的格式将来可能会更改。
 func main() {
 	client, err := ws.Connect(context.Background(), rpc.MainNetBeta_WS)
 	if err != nil {
 		panic(err)
 	}
 	program := solana.MustPublicKeyFromBase58("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin") // serum
+
+	// todo:实现了吗？？？？？？？？？？？？
 	option := ws.BlockSubscribeOpts{
-		Commitment:                     "",
+		Commitment:                     rpc.CommitmentFinalized,
 		Encoding:                       solana.EncodingBase64,
-		TransactionDetails:             "",
+		TransactionDetails:             rpc.TransactionDetailsFull,
 		Rewards:                        nil,
 		MaxSupportedTransactionVersion: nil,
 	}
